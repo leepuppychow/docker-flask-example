8634b712da4164eea1f211200fe4a1f0bd43f341
Lee Chow <leepuppychow@gmail.com>
2019-06-18 14:19:14 -0600

    Add another test route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def welcome():
     return "WELCOME TO TEST DOCKER-FLASK APP"
 
+@app.route("/index")
+def index():
+    return "Index page"
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
